7f0c699a4abf79c2cb08dbbcd9a770031819c093
Erin <150363827+ErinMenziesPluer@users.noreply.github.com>
2023-11-17 11:44:39 -0500

    Update Example.R

--- R_pipelines/HeatWaveFinder/Example.R
+++ R_pipelines/HeatWaveFinder/Example.R
@@ -4,8 +4,8 @@
 #set your working directory
 setwd("C:\\Users\\wtpluer\\Desktop\\ERA Daily Data")
 
-#you  need arrow to run the function and ggplot2 to plot the outputs like I did here
-#if you don't have these packages, install them using install.packages(c("arrow","ggplot2")
+#I need arrow to pull in a parquet file and ggplot2 to plot the outputs in this example
+#you don't need them to run the function 
 library(arrow)
 library(ggplot2)
 
@@ -21,8 +21,8 @@ Temp=read_parquet("Temp_southcom_2m.parquet")
 #I want to analyze the 6th column in the Temp dataframe which is mean temp values
 Mean_Summary=Heatwaves(Temp,30,3,6)
 #it will likely take a few minutes to run 
-#the function returns a dataframe with column headers x, y, and count. x and y are the locations in your initial dataset
-#count is the number of days that that location was in a heatwave during your time period of interest
+#the function returns a dataframe with column headers x, y, and HeatwaveDays. x and y are the locations in your initial dataset
+#HeatwaveDays is the number of days that that location was in a heatwave during your time period of interest
 
 #Optional, plot the output, here we use ggplot2 and put x and y on their respective axes and the color of the point 
 #indicates how many days that location was in a heatwave
